44e0f2535d8e732920e417628e2edaebcd92c09e
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-07-12 08:46:57 +0545

    df

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectToDb() {
   // await mongoose.connect("mongodb+srv://prabin1025y:prabin1025y@nodejs.0t2ijxr.mongodb.net/?retryWrites=true&w=majority&appName=NodeJs");
-  await mongoose.connect("mongodb://localhost:27017/blog");
+  await mongoose.connect(process.env.MONGODB_URL);
   console.log("Database Connected");
 }
 
